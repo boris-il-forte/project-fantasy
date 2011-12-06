f6b7ddca86841ceec2d4b1c99357cf61ef9dc5c5
gionnico <gionnico>
2011-12-06 12:49:27 +0100

    - controllo risultato fread e fwrite

--- src/fantasy-loadsave.c
+++ src/fantasy-loadsave.c
@@ -20,19 +20,29 @@
 #include <string.h>
 #include "fantasy-core.h"
 
+void ckfread(void *ptr, size_t size, size_t nitems, FILE *stream)
+{
+	if(fread(ptr,size,nitems,stream) < nitems) { perror("fread non riuscita"); exit(1); }
+}
+
+void ckfwrite(void *ptr, size_t size, size_t nitems, FILE *stream)
+{
+	if(fwrite(ptr,size,nitems,stream) < nitems) { perror("fwrite non riuscita"); exit(1); }
+}
+
 t_lista_t * inserisci_truppe_in_coda(t_lista_t *testa,FILE *fp, char from) //# stavolta faccio notare che è meglio in questa funzione fare solo l'inserimento e non cercare la coda! inutile cercare la coda in quanto nessun'altra parte del codice usa la suddetta funzione.
 {
 	t_lista_t *temp, *nuova;
 	nuova=malloc(sizeof(t_lista_t));
 	nuova->truppa=malloc(sizeof(t_infotruppa));
 	
-	fread(&nuova->truppa->tipo,sizeof(nuova->truppa->tipo),1,fp);
-	fread(&nuova->truppa->giocatore,sizeof(nuova->truppa->giocatore),1,fp);
-	fread(&nuova->truppa->numero,sizeof(nuova->truppa->numero),1,fp);
-	fread(&nuova->truppa->morale,sizeof(nuova->truppa->morale),1,fp);
-	fread(&nuova->truppa->stanca,sizeof(nuova->truppa->stanca),1,fp);
-	fread(&nuova->truppa->combattuto,sizeof(nuova->truppa->combattuto),1,fp);
-	fread(&nuova->pos,sizeof(nuova->pos),1,fp);
+	ckfread(&nuova->truppa->tipo,sizeof(nuova->truppa->tipo),1,fp);
+	ckfread(&nuova->truppa->giocatore,sizeof(nuova->truppa->giocatore),1,fp);
+	ckfread(&nuova->truppa->numero,sizeof(nuova->truppa->numero),1,fp);
+	ckfread(&nuova->truppa->morale,sizeof(nuova->truppa->morale),1,fp);
+	ckfread(&nuova->truppa->stanca,sizeof(nuova->truppa->stanca),1,fp);
+	ckfread(&nuova->truppa->combattuto,sizeof(nuova->truppa->combattuto),1,fp);
+	ckfread(&nuova->pos,sizeof(nuova->pos),1,fp);
 	if(from == 0) {// 0 = esterno; 1 = struttura
 		infomappa.truppe[nuova->pos]=nuova->truppa; // assegnazione "di comodo"
 	}
@@ -58,8 +68,8 @@ t_lista_s * inserisci_strutture_in_coda(t_lista_s *testa,FILE *fp)
 	t_lista_s *temp, *nuova;
 	nuova=malloc(sizeof(t_lista_s));
 
-	fread(&nuova->pos,sizeof(nuova->pos),1,fp);
-	fread(&num_truppestruttura,sizeof(num_truppestruttura),1,fp); // intruso .. che ci vuoi fare? che nassa!
+	ckfread(&nuova->pos,sizeof(nuova->pos),1,fp);
+	ckfread(&num_truppestruttura,sizeof(num_truppestruttura),1,fp); // intruso .. che ci vuoi fare? che nassa!
 	nuova->in=NULL;
 	for(l=0;l<num_truppestruttura;l++) { // *in (scorri)
 		nuova->in=inserisci_truppe_in_coda(nuova->in,fp,1);
@@ -91,9 +101,9 @@ int carica(char *nomefile)
 		return 1;
 	}
 	// INIZIO controllo header standard: FC1 (1 come intero)
-	fread(ver,1,2,fp);
+	ckfread(ver,1,2,fp);
 	ver[2]='\0';
-	fread(&ver[3],1,1,fp);
+	ckfread(&ver[3],1,1,fp);
 	if(strcmp(ver,"FC") != 0) {
 		fprintf(stderr,"MIME type sconosciuto. Intestazione errata: %c%c\n",ver[0],ver[1]);
 		return 1;
@@ -104,38 +114,38 @@ int carica(char *nomefile)
 	}
 	// FINE controllo header standard
 	// Cx,Cy
-	fread(&cx,sizeof(cx),1,fp);
-	fread(&cy,sizeof(cy),1,fp);
+	ckfread(&cx,sizeof(cx),1,fp);
+	ckfread(&cy,sizeof(cy),1,fp);
 	// infomappa
-	fread(&infomappa.castelli,sizeof(infomappa.castelli),1,fp);
-	fread(&infomappa.fattorie,sizeof(infomappa.fattorie),1,fp);
-	fread(&infomappa.stalle,sizeof(infomappa.stalle),1,fp);
-	fread(&infomappa.grotte,sizeof(infomappa.grotte),1,fp);
-	fread(&infomappa.nidi,sizeof(infomappa.nidi),1,fp);
-	fread(&infomappa.numstalle,sizeof(infomappa.numstalle),1,fp);
-	fread(&infomappa.numnidi,sizeof(infomappa.numnidi),1,fp);
-	fread(&infomappa.numgrotte,sizeof(infomappa.numgrotte),1,fp);
-	fread(&infomappa.numfattorie,sizeof(infomappa.numfattorie),1,fp);
+	ckfread(&infomappa.castelli,sizeof(infomappa.castelli),1,fp);
+	ckfread(&infomappa.fattorie,sizeof(infomappa.fattorie),1,fp);
+	ckfread(&infomappa.stalle,sizeof(infomappa.stalle),1,fp);
+	ckfread(&infomappa.grotte,sizeof(infomappa.grotte),1,fp);
+	ckfread(&infomappa.nidi,sizeof(infomappa.nidi),1,fp);
+	ckfread(&infomappa.numstalle,sizeof(infomappa.numstalle),1,fp);
+	ckfread(&infomappa.numnidi,sizeof(infomappa.numnidi),1,fp);
+	ckfread(&infomappa.numgrotte,sizeof(infomappa.numgrotte),1,fp);
+	ckfread(&infomappa.numfattorie,sizeof(infomappa.numfattorie),1,fp);
 	// END infomappa
-	fread(&num_giocatori,sizeof(num_giocatori),1,fp);
+	ckfread(&num_giocatori,sizeof(num_giocatori),1,fp);
 	for(i=0;i<num_giocatori;i++) { // *giocatore
 		giocatore[i]=malloc(sizeof(t_player));
 		for(j=0;j<NUMSTRUTTURE; j++)
 			giocatore[i]->struttura[j]=NULL;
 		for(j=0;j<NUMSTRUTTURE; j++) { // **struttura
-			fread(&num_strutture,sizeof(num_strutture),1,fp);
+			ckfread(&num_strutture,sizeof(num_strutture),1,fp);
 			for(k=0;k<num_strutture;k++) { // *struttura (scorri)
 					giocatore[i]->struttura[j]=inserisci_strutture_in_coda(giocatore[i]->struttura[j],fp);
 			} // END *struttura
 		}
-		fread(&num_truppe,sizeof(num_truppe),1,fp);
+		ckfread(&num_truppe,sizeof(num_truppe),1,fp);
 		giocatore[i]->truppe=NULL;
 		for(j=0;j<num_truppe;j++) { // *truppe (scorri)
 			giocatore[i]->truppe=inserisci_truppe_in_coda(giocatore[i]->truppe,fp,0);
 		} // END *truppe
-		fread(&giocatore[i]->oro,sizeof(giocatore[i]->oro),1,fp);
-		fread(&giocatore[i]->cibo,sizeof(giocatore[i]->cibo),1,fp);
-		fread(&giocatore[i]->smeraldi,sizeof(giocatore[i]->smeraldi),1,fp);
+		ckfread(&giocatore[i]->oro,sizeof(giocatore[i]->oro),1,fp);
+		ckfread(&giocatore[i]->cibo,sizeof(giocatore[i]->cibo),1,fp);
+		ckfread(&giocatore[i]->smeraldi,sizeof(giocatore[i]->smeraldi),1,fp);
 	} // END *giocatore
 	while(i<MAXGIOCATORI) {
 		giocatore[i]=NULL;
@@ -165,25 +175,25 @@ int salva(char *nomefile)
 		perror("fopen fallita");
 		return 1;
 	}
-	fwrite(ver,1,3,fp);
+	ckfwrite(ver,1,3,fp);
 
 	// Cx,Cy
-	fwrite(&cx,sizeof(cx),1,fp);
-	fwrite(&cy,sizeof(cy),1,fp);
+	ckfwrite(&cx,sizeof(cx),1,fp);
+	ckfwrite(&cy,sizeof(cy),1,fp);
 	// infomappa
-	fwrite(&infomappa.castelli,sizeof(infomappa.castelli),1,fp);
-	fwrite(&infomappa.fattorie,sizeof(infomappa.fattorie),1,fp);
-	fwrite(&infomappa.stalle,sizeof(infomappa.stalle),1,fp);
-	fwrite(&infomappa.grotte,sizeof(infomappa.grotte),1,fp);
-	fwrite(&infomappa.nidi,sizeof(infomappa.nidi),1,fp);
-	fwrite(&infomappa.numstalle,sizeof(infomappa.numstalle),1,fp);
-	fwrite(&infomappa.numnidi,sizeof(infomappa.numnidi),1,fp);
-	fwrite(&infomappa.numgrotte,sizeof(infomappa.numgrotte),1,fp);
-	fwrite(&infomappa.numfattorie,sizeof(infomappa.numfattorie),1,fp);
+	ckfwrite(&infomappa.castelli,sizeof(infomappa.castelli),1,fp);
+	ckfwrite(&infomappa.fattorie,sizeof(infomappa.fattorie),1,fp);
+	ckfwrite(&infomappa.stalle,sizeof(infomappa.stalle),1,fp);
+	ckfwrite(&infomappa.grotte,sizeof(infomappa.grotte),1,fp);
+	ckfwrite(&infomappa.nidi,sizeof(infomappa.nidi),1,fp);
+	ckfwrite(&infomappa.numstalle,sizeof(infomappa.numstalle),1,fp);
+	ckfwrite(&infomappa.numnidi,sizeof(infomappa.numnidi),1,fp);
+	ckfwrite(&infomappa.numgrotte,sizeof(infomappa.numgrotte),1,fp);
+	ckfwrite(&infomappa.numfattorie,sizeof(infomappa.numfattorie),1,fp);
 	// END infomappa
 	for(i=0;i<MAXGIOCATORI && giocatore[i] != NULL;i++); // *giocatore (conta)
 	num_giocatori=i;
-	fwrite(&num_giocatori,sizeof(num_giocatori),1,fp);
+	ckfwrite(&num_giocatori,sizeof(num_giocatori),1,fp);
 	for(i=0;i<num_giocatori;i++) { // *giocatore (scorri)
 		for(j=0;j<NUMSTRUTTURE; j++) { // **struttura
 			Sptr=giocatore[i]->struttura[j];
@@ -191,25 +201,25 @@ int salva(char *nomefile)
 				Sptr = Sptr->next;
 			}
 			num_strutture=k;
-			fwrite(&num_strutture,sizeof(num_strutture),1,fp);
+			ckfwrite(&num_strutture,sizeof(num_strutture),1,fp);
 			Sptr=giocatore[i]->struttura[j];
 			for(k=0;k<num_strutture;k++) { // *struttura (scorri)
-				fwrite(&Sptr->pos,sizeof(Sptr->pos),1,fp);
+				ckfwrite(&Sptr->pos,sizeof(Sptr->pos),1,fp);
 				Sptrbis=Sptr->in;
 				for(l=0; Sptrbis != NULL; l++) { // *in (conta lista)
 					Sptrbis = Sptrbis->next;
 				}
 				num_truppestruttura=l;
-				fwrite(&num_truppestruttura,sizeof(num_truppestruttura),1,fp);
+				ckfwrite(&num_truppestruttura,sizeof(num_truppestruttura),1,fp);
 				Sptrbis=Sptr->in;
 				for(l=0;l<num_truppestruttura;l++) { // *in (scorri)
-					fwrite(&Sptrbis->truppa->tipo,sizeof(Sptrbis->truppa->tipo),1,fp);
-					fwrite(&Sptrbis->truppa->giocatore,sizeof(Sptrbis->truppa->giocatore),1,fp);
-					fwrite(&Sptrbis->truppa->numero,sizeof(Sptrbis->truppa->numero),1,fp);
-					fwrite(&Sptrbis->truppa->morale,sizeof(Sptrbis->truppa->morale),1,fp);
-					fwrite(&Sptrbis->truppa->stanca,sizeof(Sptrbis->truppa->stanca),1,fp);
-					fwrite(&Sptrbis->truppa->combattuto,sizeof(Sptrbis->truppa->combattuto),1,fp);
-					fwrite(&Sptrbis->pos,sizeof(Sptrbis->pos),1,fp);
+					ckfwrite(&Sptrbis->truppa->tipo,sizeof(Sptrbis->truppa->tipo),1,fp);
+					ckfwrite(&Sptrbis->truppa->giocatore,sizeof(Sptrbis->truppa->giocatore),1,fp);
+					ckfwrite(&Sptrbis->truppa->numero,sizeof(Sptrbis->truppa->numero),1,fp);
+					ckfwrite(&Sptrbis->truppa->morale,sizeof(Sptrbis->truppa->morale),1,fp);
+					ckfwrite(&Sptrbis->truppa->stanca,sizeof(Sptrbis->truppa->stanca),1,fp);
+					ckfwrite(&Sptrbis->truppa->combattuto,sizeof(Sptrbis->truppa->combattuto),1,fp);
+					ckfwrite(&Sptrbis->pos,sizeof(Sptrbis->pos),1,fp);
 					Sptrbis=Sptrbis->next; 
 				} // END *in
 				Sptr = Sptr->next;
@@ -220,21 +230,21 @@ int salva(char *nomefile)
 			Tptr = Tptr->next;
 		}
 		num_truppe=j;
-		fwrite(&num_truppe,sizeof(num_truppe),1,fp);
+		ckfwrite(&num_truppe,sizeof(num_truppe),1,fp);
 		Tptr=giocatore[i]->truppe;
 		for(j=0;j<num_truppe;j++) { // *truppe (scorri)
-			fwrite(&Tptr->truppa->tipo,sizeof(Tptr->truppa->tipo),1,fp);
-			fwrite(&Tptr->truppa->giocatore,sizeof(Tptr->truppa->giocatore),1,fp);
-			fwrite(&Tptr->truppa->numero,sizeof(Tptr->truppa->numero),1,fp);
-			fwrite(&Tptr->truppa->morale,sizeof(Tptr->truppa->morale),1,fp);
-			fwrite(&Tptr->truppa->stanca,sizeof(Tptr->truppa->stanca),1,fp);
-			fwrite(&Tptr->truppa->combattuto,sizeof(Tptr->truppa->combattuto),1,fp);
-			fwrite(&Tptr->pos,sizeof(Tptr->pos),1,fp);
+			ckfwrite(&Tptr->truppa->tipo,sizeof(Tptr->truppa->tipo),1,fp);
+			ckfwrite(&Tptr->truppa->giocatore,sizeof(Tptr->truppa->giocatore),1,fp);
+			ckfwrite(&Tptr->truppa->numero,sizeof(Tptr->truppa->numero),1,fp);
+			ckfwrite(&Tptr->truppa->morale,sizeof(Tptr->truppa->morale),1,fp);
+			ckfwrite(&Tptr->truppa->stanca,sizeof(Tptr->truppa->stanca),1,fp);
+			ckfwrite(&Tptr->truppa->combattuto,sizeof(Tptr->truppa->combattuto),1,fp);
+			ckfwrite(&Tptr->pos,sizeof(Tptr->pos),1,fp);
 			Tptr=Tptr->next;
 		} // END *truppe
-		fwrite(&giocatore[i]->oro,sizeof(giocatore[i]->oro),1,fp);
-		fwrite(&giocatore[i]->cibo,sizeof(giocatore[i]->cibo),1,fp);
-		fwrite(&giocatore[i]->smeraldi,sizeof(giocatore[i]->smeraldi),1,fp);
+		ckfwrite(&giocatore[i]->oro,sizeof(giocatore[i]->oro),1,fp);
+		ckfwrite(&giocatore[i]->cibo,sizeof(giocatore[i]->cibo),1,fp);
+		ckfwrite(&giocatore[i]->smeraldi,sizeof(giocatore[i]->smeraldi),1,fp);
 	} // END *giocatore
 		
 	fclose(fp);
